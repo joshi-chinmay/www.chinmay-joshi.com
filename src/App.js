32383295a6a8db333e539e3fc8aae12af12d7da8
joshi-chinmay <chinmayj93@gmail.com>
2019-10-03 23:33:10 -0400

    minor errors fixed while rendering components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      Hello Chinmay.
-    </div>
-  );
+import Loadable from 'react-loadable';
+import Loading from './components/Loading.jsx';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+import "./styles/Custom.scss";
+
+const LoadableHome = Loadable({ loader: () => import('./components/Home.jsx'), loading: Loading});
+
+library.add(fab, fas);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <Switch>
+          <Route exact path={'/'} component={LoadableHome} />
+          <Route path={'/home'} component={LoadableHome} />
+
+          <Route component={LoadableHome} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class Loading extends Component {
+  render() {
+    return(
+      <div className="row">
+        <div className="col-md-12 text-center mt-5">
+          <h4>loading...</h4>
+        </div>
+      </div>
+    );
+  }
+}
