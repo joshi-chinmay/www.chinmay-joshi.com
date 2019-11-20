8eaeaa6e7a3277f6b17182ab5379d989542f85f6
joshi-chinmay <chinmayj93@gmail.com>
2019-11-19 20:25:00 -0500

    fixes on about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,11 +7,14 @@ export default class About extends Component {
         <div className="row mt-2">
           <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
             <h1 className="lead-lg">Chinmay Joshi</h1>
-            <span>UI-UX Designer | Frontend Engineer</span>
-
-          <a href="mailto:chinmayj93@gmail.com">chinmayj93@gmail.com</a>
+            <p>UI-UX Designer | Frontend Engineer</p>
+            <a href="mailto:chinmayj93@gmail.com">
+              chinmayj93@gmail.com
+            </a>
             &nbsp;&nbsp;
-            <a href="tel:+14156761018">+1-(415)-676-1018</a>
+            <a href="tel:+14156761018">
+              +1-(415)-676-1018
+            </a>
           </div>
 
           <div className="col-md-12 col-lg-6 offset-lg-3 mt-5">
@@ -22,25 +25,22 @@ export default class About extends Component {
               <div className="card-body">
                 <ul>
                   <li>
-                    <b>Frontend</b> - HTML, CSS, JavaScript, React.JS, Backbone.JS, Marionette.JS, and Ember.JS
+                    <b>Creative</b> - Adobe Creative Suite, Pretotyping, Prototyping, wireframing, user stories, and UX flows. Sharpie and sticky notes are my favorite tools as a UX designer!
                   </li>
                   <li>
-                    <b>Backend</b> - Ruby, Ruby On Rails, Python, and Node.JS
+                    <b>Frontend</b> - HTML, CSS, JavaScript, React.JS, Backbone.JS, Marionette.JS, and Ember.JS.
                   </li>
                   <li>
-                    <b>Databases</b> - MongoDB, MySQL, and DynamoDB
+                    <b>Backend</b> - Ruby, Ruby On Rails, Python, and Node.JS.
                   </li>
                   <li>
-                    <b>Cloud</b> - Amazon Web Services and Serverless
+                    <b>Databases</b> - MongoDB, MySQL, and DynamoDB.
                   </li>
                   <li>
-                    <b>Project Management Tools</b> - Git, JIRA, Confluence, Trello, CI and CD Tools
+                    <b>Cloud</b> - Amazon Web Services and Serverless.
                   </li>
                   <li>
-                    <b>Creative</b> - Prototyping, wireframing, user stories, and UX flows
-                  </li>
-                  <li>
-                    Sharpie and sticky notes are my favorite tools as a UX designer!
+                    <b>Project Management Tools</b> - Git, JIRA, Confluence, Trello, CI and CD Tools.
                   </li>
                 </ul>
               </div>
