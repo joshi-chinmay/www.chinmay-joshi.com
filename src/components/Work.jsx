a8f182c38b38fdde57ca5697b5391f84f192afa2
joshi-chinmay <chinmayj93@gmail.com>
2020-07-31 14:28:22 -0700

    removed unwanted tabs and related states and props

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -16,20 +16,6 @@ import SupahandsWebsiteImg from '../images/illustrations/supahands-website.svg';
 import AgentsWebsiteImg from '../images/illustrations/website-general.svg';
 
 export default class Work extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeWorkTab: "all",
-      workItems: this.workPageMetaData()
-    }
-
-    this.toggleWorkTabs = this.toggleWorkTabs.bind(this);
-  }
-
-  toggleWorkTabs(state) {
-
-  }
-
   workPageMetaData() {
     return([
       {
@@ -99,7 +85,6 @@ export default class Work extends Component {
   render() {
     let cards = [];
     const cardData = this.workPageMetaData();
-    const activeWorkTab = this.state.activeWorkTab;
 
     for (let i = 0; i < cardData.length; i++) {
       cards.push(
@@ -129,22 +114,6 @@ export default class Work extends Component {
           <svg xlink="http://www.w3.org/1999/xlink" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 310 350">
             <path d="M156.4,339.5c31.8-2.5,59.4-26.8,80.2-48.5c28.3-29.5,40.5-47,56.1-85.1c14-34.3,20.7-75.6,2.3-111  c-18.1-34.8-55.7-58-90.4-72.3c-11.7-4.8-24.1-8.8-36.8-11.5l-0.9-0.9l-0.6,0.6c-27.7-5.8-56.6-6-82.4,3c-38.8,13.6-64,48.8-66.8,90.3c-3,43.9,17.8,88.3,33.7,128.8c5.3,13.5,10.4,27.1,14.9,40.9C77.5,309.9,111,343,156.4,339.5z"/>
           </svg>
-        </div>
-
-        <div className="row">
-          <div className="col-md-12">
-            <ul className="nav secondary-nav justify-content-center">
-              <li className="nav-item px-3">
-                <button className={"btn btn-nav-link nav-link " + activeWorkTab === "add" ? "active" : "" }>All</button>
-              </li>
-              <li className="nav-item mx-3">
-                <button className="btn btn-nav-link nav-link">Academic</button>
-              </li>
-              <li className="nav-item mx-3">
-                <button className="btn btn-nav-link nav-link">Professional</button>
-              </li>
-            </ul>
-          </div>
         </div>
 
         <div className="row">
