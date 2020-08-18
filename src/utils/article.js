78737bf430150fed89897baa7783521880d45b1b
joshi-chinmay <chinmayj93@gmail.com>
2020-08-17 19:43:32 -0700

    article file formatting

--- src/utils/article.js
+++ src/utils/article.js
@@ -9,9 +9,9 @@ const getArticle = async () => {
   document.title = data.title;
   document.querySelector('#title').textContent = data.title;
   if(data.cover_image){
-      document.querySelector('#cover').src = data.cover_image;
-  }else{
-      document.querySelector('#cover').style.display = 'none';
+    document.querySelector('#cover').src = data.cover_image;
+  } else {
+    document.querySelector('#cover').style.display = 'none';
   }
 
   document.querySelector('#author-img').src = data.user.profile_image_90;
@@ -23,33 +23,34 @@ const getArticle = async () => {
   setUserLink('#devto', !!data.user.username, `https://dev.to/${data.user.username}`);
   setUserLink('#homepage', !!data.user.website_url, data.user.website_url);
   if(data.user.website_url){
-      document.querySelector('#homepage').textContent = data.user.website_url.replace(/http[s]?:\/\//, '');
+    document.querySelector('#homepage').textContent = data.user.website_url.replace(/http[s]?:\/\//, '');
   }
 
   document.querySelector('#date').textContent = data.readable_publish_date;
 
   const tagList = document.querySelector('#tags');
   for(const tag of data.tags){
-      const li = document.createElement('li');
-      li.textContent = tag;
-      tagList.appendChild(li);
+    const li = document.createElement('li');
+    li.textContent = tag;
+    tagList.appendChild(li);
   }
 
   document.querySelector('#article-body').innerHTML = data.body_html.replace(/\/assets\//g, './assets/');
   document.querySelectorAll('blockquote').forEach(e => {
-      e.addEventListener('click', ({target}) => {
-          if (!target.href && !target.parentElement.href){
-              window.open(e.dataset.url, '_blank');
-          }
-      })
+    e.addEventListener('click', ({target}) => {
+      if (!target.href && !target.parentElement.href){
+        window.open(e.dataset.url, '_blank')
+        
+      }
+    })
   });
 }
 
 const setUserLink = (id, hasValue, url) => {
   const el = document.querySelector(id);
   if (!hasValue){
-      el.style.display = 'none';
-      return;
+    el.style.display = 'none';
+    return;
   }
   el.href = url;
 }
